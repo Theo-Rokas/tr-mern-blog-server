4a79b8c97d7189296d1305552aa3ed5f0a76327e
Theo-Rokas <T.Rokas@hotmail.com>
2022-09-19 11:01:38 +0300

    First Commit

--- index.js
+++ index.js
@@ -39,6 +39,6 @@ app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
 app.use("/api/posts", postRoute)
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || "5000", () => {
     console.log("Backend is running.")
 })
