d22b2554969e2b6a93aea25dd55af91bf77fc332
Theo-Rokas <T.Rokas@hotmail.com>
2022-09-19 12:36:29 +0300

    First Commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config()
 app.use(express.json())
 app.use("/images", express.static(path.join(__dirname, "/images")))
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
